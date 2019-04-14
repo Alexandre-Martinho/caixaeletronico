8789620d682d152611a56e70cd67c00f02f555c2
alexandre-martinho <alemartinho7@yahoo.com.br>
2019-04-14 12:40:59 -0300

    Enviando os arquivos originais do repositorio local

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+try {
+//	$pdo = new PDO("mysql:dbname=projeto_caixaeletronico;host=localhost", "root", "root");
+	$pdo = new PDO("mysql:dbname=caixaeletronico;host=localhost", "root", "");
+} catch(PDOException $e) {
+	echo "ERRO: ".$e->getMessage();
+	exit;
+}
+
+?>
